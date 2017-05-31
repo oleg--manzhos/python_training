7735a71885657998a3f2731aa9abc89f337fe014
Oleg Manzhos <oleg.manzhos@gmail.com>
2017-05-31 17:47:01 +0200

    Common fixture was created

--- tests/contact_add_test.py
+++ tests/contact_add_test.py
@@ -1,14 +1,4 @@
 # -*- coding: utf-8 -*-
-import pytest
-
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_contact_add(app):
